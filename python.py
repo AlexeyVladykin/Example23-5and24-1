bc5c4b63628e765142e457060a3836b12dc05dd3
AlexKraun <zachitnik20016@gmail.com>
2026-03-16 15:14:55 +0300

    Create python.py

---
+++ python.py
@@ -0,0 +1,34 @@
+def Summ(a, b):
+    print(f"")
+
+def Minus(a, b):
+    print(f"")
+
+def Mult(a, b):
+    print("")
+
+def Delenie(a, b):
+    print("")
+
+
+chisloA = int(input("Введите число А: "))
+chisloB = int(input("Введите число Б: "))
+
+print("Выберите действие:\n1. Сложение\n2. Вычитание\n3. Умножение\n4. Деление\n")
+
+deistvie = input()
+
+if deistvie.lower() == "1" or "сложение":
+    Summ(chisloA, chisloB)
+
+elif deistvie.lower() == "2" or "вычитание":
+    Minus(chisloA, chisloB)
+
+elif deistvie.lower() == "3" or "умножение":
+    Mult(chisloA, chisloB)
+
+elif deistvie.lower() == "4" or "деление":
+    Delenie(chisloA, chisloB)
+
+else:
+    print("Введены некорректные данные или выбрано некорректное действие!")
